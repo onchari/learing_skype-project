5cdbc6e8cdd0666ca1d3ad1559a735be135ce74d
onchari wycliffe <onchariwycliffe@gmail.com>
2018-03-07 15:26:53 +0300

    Before I start coding

---
+++ learning skype database/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_skype_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
